835213e3c0021a04f8f932b2fffe9b9afcd61740
Shiyu Du <dsy1998@live.com>
2016-10-24 09:31:57 -0400

    Change temp directory management strategy. Remove only the files the program will be writing. Before exiting, remove the files the program produced when running.

--- Revolutionary Hello World/main.swift
+++ Revolutionary Hello World/main.swift
@@ -12,37 +12,30 @@ let manager = FileManager.default
 
 let workingDirectoryPath = manager.urls(for: .cachesDirectory, in: .userDomainMask).first!.path.appending(pathComponent: "Revolutionary Hello World")
 
-if !manager.fileExists(atPath: workingDirectoryPath) {
-    do {
-        try manager.createDirectory(atPath: workingDirectoryPath, withIntermediateDirectories: true, attributes: nil)
-    } catch {
-        print(error.localizedDescription)
-        exit(100)
-    }
-} else {
-    do {
-        let contents = try manager.contentsOfDirectory(atPath: workingDirectoryPath)
-        for file in contents {
-            try manager.removeItem(atPath: file)
-        }
-    } catch {
-        print(error.localizedDescription)
-        exit(100)
-    }
-}
-
 let swiftSourceExtension = "swift"
 let programName = "hello"
 let sourceFilePath = workingDirectoryPath.appending(pathComponent: programName.appending(pathExtension: swiftSourceExtension)!)
 let executableFilePath = workingDirectoryPath.appending(pathComponent: programName)
 
+let sourceText = "let name = CommandLine.arguments.count > 1 ? CommandLine.arguments[1] : \"world\"\nprint(\"Hello, \\(name)!\")"
+
 do {
-    print("Writing program source...")
+    if !manager.fileExists(atPath: workingDirectoryPath) {
+        try manager.createDirectory(atPath: workingDirectoryPath, withIntermediateDirectories: true, attributes: nil)
+    }
     
-    try "let name = CommandLine.arguments.count > 1 ? CommandLine.arguments[1] : \"world\"\nprint(\"Hello, \\(name)!\")".write(toFile: sourceFilePath, atomically: true, encoding: String.Encoding.utf8)
+    if manager.fileExists(atPath: sourceFilePath) {
+        try manager.removeItem(atPath: sourceFilePath)
+    }
+    if manager.fileExists(atPath: executableFilePath) {
+        try manager.removeItem(atPath: executableFilePath)
+    }
+    
+    print("Writing program source...")
+    try sourceText.write(toFile: sourceFilePath, atomically: true, encoding: .utf8)
 } catch {
-    print(error.localizedDescription)
-    exit(1)
+    print(error)
+    exit(100)
 }
 
 launch("/usr/bin/xcrun", with: ["-sdk", "macosx", "swiftc", sourceFilePath, "-o", executableFilePath], from: workingDirectoryPath).waitUntilExit()
@@ -50,3 +43,10 @@ launch("/usr/bin/xcrun", with: ["-sdk", "macosx", "swiftc", sourceFilePath, "-o"
 let args: [String]? = CommandLine.arguments.count > 1 ? CommandLine.arguments.removingFirst : nil
 print("Running program: \(programName), with arguments: \((args ?? []).description))")
 launch(executableFilePath, with: args).waitUntilExit()
+
+do {
+    try manager.removeItem(atPath: sourceFilePath)
+    try manager.removeItem(atPath: executableFilePath)
+} catch {
+    print(error)
+}
